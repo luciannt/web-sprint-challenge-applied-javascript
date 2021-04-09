845198dc201433a3e5ddaba0c775499c4d290399
Lucianna Tittle <lucianna.roset@gmail.com>
2021-04-09 13:56:47 -0500

    Working on Task 1 and 2.

--- src/components/header.js
+++ src/components/header.js
@@ -10,8 +10,21 @@ const Header = (title, date, temp) => {
   //    <h1>{ title }</h1>
   //    <span class="temp">{ temp }</span>
   //  </div>
-  //
-}
+
+  const header = document.createElement("div");
+  const date = document.createElement("span");
+  const title = document.createElement("h1");
+  const temp = document.createElement("span");
+
+  header.classList.add("header");
+  date.classList.add("date");
+  temp.classList.add("temp");
+
+  document.appendChild(header);
+  header.appendChild(date);
+  header.appendChild(title);
+  header.appendChild(temp);
+};
 
 const headerAppender = (selector) => {
   // TASK 2
@@ -20,6 +33,6 @@ const headerAppender = (selector) => {
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
-}
+};
 
-export { Header, headerAppender }
+export { Header, headerAppender };
